a8df500e2c3bcf9d4c8d84fcc55bd1ed99272e5b
Ramesh Polishetti <4971903+rpolishetti@users.noreply.github.com>
2023-09-22 00:51:04 -0700

    tailwind config

--- src/client/components/home.jsx
+++ src/client/components/home.jsx
@@ -4,9 +4,6 @@ import styles from './home.module.css';
 export default () => {
   return (
     <div className={styles.cntr}>
-      <span className='test'>
-          test
-      </span>
       Home Component...25
     </div>
   )
